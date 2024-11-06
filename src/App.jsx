d45f717a0b9f548b93a8e1fbcc1eaa59faf5ffea
mohammed kaif <mkaif555123@gmail.com>
2024-11-06 18:57:15 +0500

    project build start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Product from './components/Product'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Ecommerce Store Website</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
