205132ed86f5eca81a7b474eaa1e6246b3221b63
mohammed kaif <mkaif555123@gmail.com>
2024-11-07 15:37:02 +0500

    navbar 100% Work Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,8 @@
+import { CiSearch } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import { IoCartOutline } from "react-icons/io5";
+import { HiMiniUserCircle } from "react-icons/hi2";
+
 function Navbar() {
     return (
         <>
@@ -5,7 +10,8 @@ function Navbar() {
             items-center gap-32">
                 <div>
                     <h1 className="text-2xl font-bold">Exclusive</h1>
-                </div>  
+                </div>
+
                 <div>
                     <ul className="flex gap-12 text-base">
                         <li>Home</li>
@@ -14,7 +20,26 @@ function Navbar() {
                         <li>Sign Up</li>
                     </ul>
                 </div>
-                <div></div>
+
+                <div className="flex gap-6">
+                    <div className="flex justify-center items-center relative">
+                        <input
+                            className="w-60 h-9 rounded py-2
+                        px-3 text-xs pr-10 bg-neutral-100"
+                            placeholder="What are you looking for?"
+                            type="search"
+                        />
+                        <CiSearch className="absolute right-2 top-1/2 transform
+                        -translate-y-1/2 text-black text-2xl" />
+                    </div>
+                    <div className="flex gap-4 items-center text-3xl">
+                        <CiHeart />
+                        <IoCartOutline />
+                        <HiMiniUserCircle className="text-red-500 font-bold" />
+
+                    </div>
+                </div>
+
             </div>
 
         </>
