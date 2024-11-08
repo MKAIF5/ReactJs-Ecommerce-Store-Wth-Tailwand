5b3e37a97a419a1d72a0de1f2a650715e601b088
mohammed kaif <mkaif555123@gmail.com>
2024-11-08 14:44:20 +0500

    home page 50% Work Completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,22 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 function Home() {
     return (
         <>
+            <br />  <br />
             <div className="flex justify-center">
-                <div>
-
-                    <div className="leading-9">
-                        <p>Woman’s Fashion<MdOutlineKeyboardArrowRight /></p>
-                        <p>Men’s Fashion</p>
-                        <p>Electronics</p>
-                        <p>Home & Lifestyle</p>
-                        <p>Medicine</p>
-                        <p>Sports & Outdoor</p>
-                        <p>Baby’s & Toys</p>
-                        <p>Groceries & Pets</p>
-                        <p>Health & Beauty</p>
-                    </div>
+                <div className="leading-9">
+                    <p className="flex
+                     items-center gap-12">Woman’s Fashion <MdOutlineKeyboardArrowRight
+                            className="text-2xl" /></p>
+                    <p
+                        className="flex items-center gap-20">Men’s Fashion
+                        <MdOutlineKeyboardArrowRight className="text-2xl" /> </p>
+                    <p>Electronics</p>
+                    <p>Home & Lifestyle</p>
+                    <p>Medicine</p>
+                    <p>Sports & Outdoor</p>
+                    <p>Baby’s & Toys</p>
+                    <p>Groceries & Pets</p>
+                    <p>Health & Beauty</p>
                 </div>
 
 
