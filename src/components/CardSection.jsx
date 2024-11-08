864b96ca8bd51c3f59ec3936fb9a161663aaa15f
mohammed kaif <mkaif555123@gmail.com>
2024-11-08 17:31:27 +0500

    card section completed

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,3 +1,5 @@
+import { FaArrowLeft } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
 
 function CardSection() {
     return (
@@ -11,48 +13,59 @@ function CardSection() {
                 </div>
             </div>
             <br />
+            {/* Today's Section Completed */}
 
-            <div className="mx-24 flex gap-20">
-                <div>
-                    <h1 className="text-4xl font-semibold">Flash Sales</h1>
+            <div className="flex justify-around gap-72">
+                <div className="flex gap-20">
+                    <div>
+                        <h1 className="text-4xl font-semibold">Flash Sales</h1>
+                    </div>
+
+                    <div className="flex gap-5 items-center">
+                        <div>
+                            <p className="text-xs font-medium">Days</p>
+                            <h1 className="text-3xl font-bold">03</h1>
+                        </div>
+
+                        <div className="space-y-2">
+                            <div className="w-1 h-1 rounded-full bg-[#db4444]"></div>
+                            <div className="w-1 h-1 rounded-full bg-[#db4444]"></div>
+                        </div>
+
+                        <div>
+                            <p className="text-xs font-medium">Hours</p>
+                            <h1 className="text-3xl font-bold">23</h1>
+                        </div>
+                        <div className="space-y-2">
+                            <div className="w-1 h-1 rounded-full bg-[#db4444]"></div>
+                            <div className="w-1 h-1 rounded-full bg-[#db4444]"></div>
+                        </div>
+                        <div>
+                            <p className="text-xs font-medium">Minutes</p>
+                            <h1 className="text-3xl font-bold">19</h1>
+                        </div>
+                        <div className="space-y-2">
+                            <div className="w-1 h-1 rounded-full bg-[#db4444]"></div>
+                            <div className="w-1 h-1 rounded-full bg-[#db4444]"></div>
+                        </div>
+                        <div>
+                            <p className="text-xs font-medium">Seconds</p>
+                            <h1 className="text-3xl font-bold">56</h1>
+                        </div>
+                    </div>
                 </div>
-
-
-                <div className="flex gap-5 items-center">
-                    <div>
-                        <p className="text-xs font-medium">Days</p>
-                        <h1 className="text-3xl font-bold">03</h1>
+                <div className="flex gap-2">
+                    <div className="bg-gray-200 w-11 h-11 flex justify-center
+                    items-center rounded-full">
+                        <FaArrowLeft />
                     </div>
-
-                    <div className="space-y-2">
-                        <div className="w-2 h-2 rounded-full bg-[#db4444]"></div>
-                        <div className="w-2 h-2 rounded-full bg-[#db4444]"></div>
-                    </div>
-
-                    <div>
-                        <p className="text-xs font-medium">Hours</p>
-                        <h1 className="text-3xl font-bold">23</h1>
-                    </div>
-                    <div className="space-y-2">
-                        <div className="w-2 h-2 rounded-full bg-[#db4444]"></div>
-                        <div className="w-2 h-2 rounded-full bg-[#db4444]"></div>
-                    </div>
-                    <div>
-                        <p className="text-xs font-medium">Minutes</p>
-                        <h1 className="text-3xl font-bold">19</h1>
-                    </div>
-                    <div className="space-y-2">
-                        <div className="w-2 h-2 rounded-full bg-[#db4444]"></div>
-                        <div className="w-2 h-2 rounded-full bg-[#db4444]"></div>
-                    </div>
-                    <div>
-                        <p className="text-xs font-medium">Seconds</p>
-                        <h1 className="text-3xl font-bold">56</h1>
+                    <div className="bg-gray-200 w-11 h-11 flex justify-center
+                    items-center rounded-full">
+                        <FaArrowRight />
                     </div>
                 </div>
-
-
             </div>
+            {/* Flash Sales Section Cmpleted */}
 
         </>
     )
