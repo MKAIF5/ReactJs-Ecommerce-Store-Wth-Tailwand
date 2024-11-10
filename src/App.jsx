6efece0f41be0b63e8c177919c47969fd39b24e2
mohammed kaif <mkaif555123@gmail.com>
2024-11-10 19:17:18 +0500

    catogery section 30% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import Banner from './components/Banner'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import CardSection from './components/CardSection'
-import './App.css'
 import Card from './components/Card'
+import Category from './components/Category'
+import './App.css'
 
 function App() {
 
   return (
     <>
-
       <Banner />
       <Navbar />
       <Home />
       <CardSection />
       <Card />
+      <Category />
     </>
   )
 }
---
+++ src/components/Category.jsx
@@ -0,0 +1,34 @@
+import { FaArrowLeft } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
+
+function Category() {
+    return (
+        <>
+            <div className="flex gap-5 items-center mx-24">
+                <div className="w-5 h-10 bg-[#db4444] rounded-md"></div>
+                <div>
+                    <p className="font-semibold text-[#db4444]">Categories</p>
+                </div>
+            </div>
+            <br />
+
+            <div className="flex justify-between items-center mx-24">
+                <div>
+                    <h1 className="text-4xl font-semibold">Browse By Category</h1>
+                </div>
+
+                <div className="flex gap-2">
+                    <div className="bg-gray-200 w-11 h-11 flex justify-center items-center rounded-full">
+                        <FaArrowLeft />
+                    </div>
+                    <div className="bg-gray-200 w-11 h-11 flex justify-center items-center rounded-full">
+                        <FaArrowRight />
+                    </div>
+                </div>
+            </div>
+            <br />
+        </>
+    );
+}
+
+export default Category;
