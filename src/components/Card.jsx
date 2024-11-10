64285989ff9a9114fdd98f3ec7d7ac4db06212f1
mohammed kaif <mkaif555123@gmail.com>
2024-11-10 14:57:41 +0500

    card 1 completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import { CiHeart } from "react-icons/ci";
 import { FiEye } from "react-icons/fi";
+import { FaStar } from "react-icons/fa";
 import img1 from "../assets/img/game1.png"
 
 function Card() {
@@ -8,39 +9,67 @@ function Card() {
             <br />
             <div className="flex justify-center">
 
-            <div className='w-64 bg-gray-300 p-3 rounded'>
 
-                <div className="flex justify-between">
-                    <div className='text-white w-14 bg-[#db4444] flex justify-center
+
+
+
+                {/* card 1 */}
+                <div>
+
+                    <div className='w-72 bg-gray-200 p-3 rounded'>
+
+                        <div className="flex justify-between">
+                            <div className='text-white w-14 bg-[#db4444] flex justify-center
                     items-center text-xs h-6'>
-                        <p>-40%</p>
-                    </div>
-                    <div className="space-y-3">
-                        <div className="w-8 h-8 bg-white flex justify-center
+                                <p>-40%</p>
+                            </div>
+                            <div className="space-y-3">
+                                <div className="w-8 h-8 bg-white flex justify-center
                          items-center text-2xl rounded-full">
-                            <CiHeart />
+                                    <CiHeart />
+                                </div>
+
+                                <div className="w-8 h-8 bg-white flex justify-center
+                         items-center text-2xl rounded-full">
+                                    <FiEye />
+                                </div>
+                            </div>
                         </div>
 
-                        <div className="w-8 h-8 bg-white flex justify-center
-                         items-center text-2xl rounded-full">
-                            <FiEye />
+                        <div className="flex justify-center">
+                            <img src={img1} alt="" />
                         </div>
                     </div>
+                    <div className="mt-5 leading-9">
+                        <div>
+                            <p className="text-base font-medium">HAVIT HV-G92 Gamepad</p>
+                        </div>
+                        <div>
+                            <p
+                                className="text-[#db4444] flex gap-3"
+                            >$120 <span className="line-through text-gray-400">
+                                    $160</span></p>
+                        </div>
+                        <div className="flex gap-1">
+                            <FaStar className="text-yellow-400 w-5 h-5" />
+                            <FaStar className="text-yellow-400 w-5 h-5" />
+                            <FaStar className="text-yellow-400 w-5 h-5" />
+                            <FaStar className="text-yellow-400 w-5 h-5" />
+                            <FaStar className="text-yellow-400 w-5 h-5" /> 
+                            <p className="text-sm text-gray-400 ml-2">(88)</p>                        
+                        </div>
+                    </div>
+
+
+
+
+
                 </div>
 
-                <div className="flex justify-center">
-                    <img src={img1} alt="" />
-                </div>
+                {/* card 1 end */}
 
 
             </div>
-
-
-
-
-
-
-                             </div>
         </>
     )
 }
